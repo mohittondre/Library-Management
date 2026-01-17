6d4a0939a407bc35a4bc3b756faf6a30c8d7d030
mohittondre <mohittondre214@gmail.com>
2026-01-17 13:17:47 +0530

    code update

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import BrowseBooks from "./pages/BrowseBooks";
+import BookDetails from "./pages/BookDetails";
+import AddBook from "./pages/AddBook";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/:category" element={<BrowseBooks />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/add-book" element={<AddBook />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
